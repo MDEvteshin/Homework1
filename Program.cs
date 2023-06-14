df01f5e2b4a1c8e6f2cae4583ee4623eb66416a0
MIkhail <MDEvteshin@yandex.ru>
2023-06-14 19:12:00 +0400

    5

--- Program.cs
+++ Program.cs
@@ -56,3 +56,20 @@
 // {
 //     System.Console.WriteLine($"Число {num1} является нечетным");
 // }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// System.Console.WriteLine("Введите любое число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int evenNumber = 2;
+
+// if (num > 1)
+// {
+//     while (evenNumber <= num )
+//     {
+//         System.Console.Write(evenNumber + " ");
+//         evenNumber = evenNumber + 2;
+//     }
+// }
